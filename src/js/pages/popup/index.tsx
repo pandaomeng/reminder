b7da63a3cbca56002ea3f4b5d7cf8585455238e0
pandaomeng <daomeng.pan@datayes.com>
2020-05-02 22:29:10 +0800

    use notification

--- src/js/pages/popup/index.tsx
+++ src/js/pages/popup/index.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.scss';
 
 interface Props {}
 
 const Popup: React.FC<Props> = () => {
-  return <div className="container">popup page1</div>;
+  const [notification, setNotification] = useState<Notification | null>(null);
+
+  const handleClick = () => {
+    const n = new Notification('Remind me', {
+      icon: 'icon.png',
+    });
+    setNotification(n);
+  };
+
+  const handleClose = () => {
+    // eslint-disable-next-line no-unused-expressions
+    notification?.close();
+  };
+
+  return (
+    <div className="container">
+      <button type="button" onClick={handleClick}>
+        show notification
+      </button>
+      <button type="button" onClick={handleClose}>
+        close notification
+      </button>
+    </div>
+  );
 };
 
 export default Popup;
